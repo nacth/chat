c49c37f60d20a0f4d9e9bade22b32b7ab9631bbd
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-08-31 21:12:42 +0700

    add home screen

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,87 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
+import '/app/routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  final List<Widget> myChats = List.generate(20, (index) {
+    return ListTile(
+      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+      leading: CircleAvatar(
+        radius: 30,
+        backgroundColor: Colors.black26,
+        child: Image.asset(
+          "assets/logo/noimage.png",
+          fit: BoxFit.cover,
+        ),
+      ),
+      title: Text(
+        "Person ${index + 1}",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+      subtitle: Text(
+        "Status ${index + 1}",
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+      ),
+      trailing: Chip(label: Text("3")),
+    );
+  }).reversed.toList();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
+      body: Column(
+        children: [
+          Material(
+            elevation: 5,
+            child: Container(
+              margin: EdgeInsets.only(top: context.mediaQueryPadding.top),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.black38)),
+              ),
+              padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Chats",
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                  ),
+                  Material(
+                    color: Colors.red[900],
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: () => Get.toNamed(Routes.PROFILE),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.person,
+                          size: 35,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: myChats.length,
+              itemBuilder: (context, index) => myChats[index],
+            ),
+          )
+        ],
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Get.toNamed(Routes.SEARCH),
+        child: Icon(
+          Icons.search,
+          size: 30,
         ),
+        backgroundColor: Colors.red[900],
       ),
     );
   }
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
 
+import 'package:chat/app/modules/chat_room/bindings/chat_room_binding.dart';
+import 'package:chat/app/modules/chat_room/views/chat_room_view.dart';
 import 'package:chat/app/modules/home/bindings/home_binding.dart';
 import 'package:chat/app/modules/home/views/home_view.dart';
 import 'package:chat/app/modules/introduction/bindings/introduction_binding.dart';
 import 'package:chat/app/modules/introduction/views/introduction_view.dart';
 import 'package:chat/app/modules/login/bindings/login_binding.dart';
 import 'package:chat/app/modules/login/views/login_view.dart';
+import 'package:chat/app/modules/profile/bindings/profile_binding.dart';
+import 'package:chat/app/modules/profile/views/profile_view.dart';
+import 'package:chat/app/modules/search/bindings/search_binding.dart';
+import 'package:chat/app/modules/search/views/search_view.dart';
 
 part 'app_routes.dart';
 
@@ -27,6 +33,21 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => ProfileView(),
+      binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: _Paths.CHAT_ROOM,
+      page: () => ChatRoomView(),
+      binding: ChatRoomBinding(),
+    ),
+    GetPage(
+      name: _Paths.SEARCH,
+      page: () => SearchView(),
+      binding: SearchBinding(),
     ),
   ];
 }
