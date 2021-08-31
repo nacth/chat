fb13fb16f001b387fe8a59270589012e55135db7
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-08-31 20:36:17 +0700

    add login screen

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -8,14 +8,50 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: Get.width * 0.7,
+                  height: Get.width * 0.7,
+                  child: Lottie.asset("assets/lottie/login.json"),
+                ),
+                SizedBox(height: 150),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          child: Image.asset("assets/logo/google.png"),
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          "Sign In With Google",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red[900],
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -22,38 +22,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return ErrorScreen();
-        }
-        if (snapshot.connectionState == ConnectionState.done) {
-          return FutureBuilder(
-            future: Future.delayed(Duration(seconds: 3)),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Obx(
-                  () {
-                    return GetMaterialApp(
-                      debugShowCheckedModeBanner: false,
-                      title: "Chat App",
-                      initialRoute: authC.isSkipIntro.isTrue
-                          ? authC.isAuth.isTrue
-                              ? Routes.HOME
-                              : Routes.LOGIN
-                          : Routes.INTRODUCTION,
-                      getPages: AppPages.routes,
-                    );
-                  },
-                );
-              }
-              return SplashScreen();
-            },
-          );
-        }
-        return LoadingScreen();
-      },
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Chat App",
+      initialRoute: Routes.LOGIN,
+      getPages: AppPages.routes,
     );
+    // return FutureBuilder(
+    //   future: _initialization,
+    //   builder: (context, snapshot) {
+    //     if (snapshot.hasError) {
+    //       return ErrorScreen();
+    //     }
+    //     if (snapshot.connectionState == ConnectionState.done) {
+    //       return FutureBuilder(
+    //         future: Future.delayed(Duration(seconds: 3)),
+    //         builder: (context, snapshot) {
+    //           if (snapshot.connectionState == ConnectionState.done) {
+    //             return Obx(
+    //               () {
+    //                 return GetMaterialApp(
+    //                   debugShowCheckedModeBanner: false,
+    //                   title: "Chat App",
+    //                   initialRoute: authC.isSkipIntro.isTrue
+    //                       ? authC.isAuth.isTrue
+    //                           ? Routes.HOME
+    //                           : Routes.LOGIN
+    //                       : Routes.INTRODUCTION,
+    //                   getPages: AppPages.routes,
+    //                 );
+    //               },
+    //             );
+    //           }
+    //           return SplashScreen();
+    //         },
+    //       );
+    //     }
+    //     return LoadingScreen();
+    //   },
+    // );
   }
 }
