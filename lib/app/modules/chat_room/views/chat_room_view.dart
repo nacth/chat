f5b30a0e402f360e77bf32d78fcf91a0b85b7792
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-09-01 07:49:14 +0700

    add chat room screen

--- lib/app/modules/chat_room/views/chat_room_view.dart
+++ lib/app/modules/chat_room/views/chat_room_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/chat_room_controller.dart';
@@ -9,15 +8,142 @@ class ChatRoomView extends GetView<ChatRoomController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ChatRoomView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ChatRoomView is working',
-          style: TextStyle(fontSize: 20),
+        backgroundColor: Colors.red[900],
+        leadingWidth: 100,
+        leading: InkWell(
+          onTap: () => Get.back(),
+          borderRadius: BorderRadius.circular(100),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(width: 5),
+              Icon((Icons.arrow_back)),
+              SizedBox(width: 5),
+              CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.grey,
+                  child: Image.asset("assets/logo/noimage.png")),
+            ],
+          ),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Lorem Ipsum',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              'Status',
+              style: TextStyle(fontSize: 14),
+            )
+          ],
         ),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                ChatItem(isSender: true),
+                ChatItem(isSender: false),
+                ChatItem(isSender: true),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.emoji_emotions_outlined),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Material(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.red[900],
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(100),
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  const ChatItem({
+    Key? key,
+    required this.isSender,
+  }) : super(key: key);
+
+  final bool isSender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 15,
+        horizontal: 20,
+      ),
+      child: Column(
+        crossAxisAlignment:
+            isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.red[900],
+                borderRadius: isSender
+                    ? BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                        bottomLeft: Radius.circular(15),
+                      )
+                    : BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                      )),
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "Hello World!",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          SizedBox(height: 5),
+          Text("00:00 PM"),
+        ],
+      ),
+      alignment: isSender ? Alignment.centerRight : Alignment.centerLeft,
     );
   }
 }
