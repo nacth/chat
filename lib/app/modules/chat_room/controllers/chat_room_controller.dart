6cccb06168217970d5158dbd6d62e401f3a06596
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-09-01 09:05:23 +0700

    add emoji picker

--- lib/app/modules/chat_room/controllers/chat_room_controller.dart
+++ lib/app/modules/chat_room/controllers/chat_room_controller.dart
@@ -1,18 +1,37 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class ChatRoomController extends GetxController {
-  final count = 0.obs;
+  var isShowEmoji = false.obs;
+
+  late FocusNode focusNode;
+  late TextEditingController chatC;
+
+  void addEmojiToChat(Emoji emoji) {
+    chatC.text = chatC.text + emoji.emoji;
+  }
+
+  void deleteEmoji() {
+    chatC.text = chatC.text.substring(0, chatC.text.length - 2);
+  }
+
   @override
   void onInit() {
+    chatC = TextEditingController();
+    focusNode = FocusNode();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        isShowEmoji.value = false;
+      }
+    });
     super.onInit();
   }
 
   @override
-  void onReady() {
-    super.onReady();
+  void onClose() {
+    chatC.dispose();
+    focusNode.dispose();
+    super.onClose();
   }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/chat_room/views/chat_room_view.dart
+++ lib/app/modules/chat_room/views/chat_room_view.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -43,58 +46,111 @@ class ChatRoomView extends GetView<ChatRoomController> {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: [
-                ChatItem(isSender: true),
-                ChatItem(isSender: false),
-                ChatItem(isSender: true),
-              ],
+      body: WillPopScope(
+        onWillPop: () {
+          if (controller.isShowEmoji.isTrue) {
+            controller.isShowEmoji.value = false;
+          } else {
+            Navigator.pop(context);
+          }
+          return Future.value(false);
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  ChatItem(isSender: true),
+                  ChatItem(isSender: false),
+                  ChatItem(isSender: true),
+                ],
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Container(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.emoji_emotions_outlined),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
+            Container(
+              margin: EdgeInsets.only(
+                  bottom: controller.isShowEmoji.isTrue
+                      ? 5
+                      : context.mediaQueryPadding.bottom),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: TextField(
+                        controller: controller.chatC,
+                        focusNode: controller.focusNode,
+                        decoration: InputDecoration(
+                          prefixIcon: IconButton(
+                            onPressed: () {
+                              controller.focusNode.unfocus();
+                              controller.isShowEmoji.toggle();
+                            },
+                            icon: Icon(Icons.emoji_emotions_outlined),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 10),
-                Material(
-                  borderRadius: BorderRadius.circular(100),
-                  color: Colors.red[900],
-                  child: InkWell(
+                  SizedBox(width: 10),
+                  Material(
                     borderRadius: BorderRadius.circular(100),
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Icon(
-                        Icons.send,
-                        color: Colors.white,
+                    color: Colors.red[900],
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(100),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            Obx(
+              () => controller.isShowEmoji.isTrue
+                  ? Container(
+                      height: Get.height * 0.35,
+                      child: EmojiPicker(
+                        onEmojiSelected: (category, emoji) {
+                          controller.addEmojiToChat(emoji);
+                        },
+                        onBackspacePressed: () {
+                          controller.deleteEmoji();
+                        },
+                        config: Config(
+                            columns: 7,
+                            emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
+                            verticalSpacing: 0,
+                            horizontalSpacing: 0,
+                            initCategory: Category.RECENT,
+                            bgColor: Color(0xFFF2F2F2),
+                            backspaceColor: Color(0xFFB71C1C),
+                            indicatorColor: Color(0xFFB71C1C),
+                            iconColor: Colors.grey,
+                            iconColorSelected: Color(0xFFB71C1C),
+                            progressIndicatorColor: Color(0xFFB71C1C),
+                            showRecentsTab: true,
+                            recentsLimit: 28,
+                            noRecentsText: "No Recents",
+                            noRecentsStyle: const TextStyle(
+                                fontSize: 20, color: Colors.black26),
+                            tabIndicatorAnimDuration: kTabScrollDuration,
+                            categoryIcons: const CategoryIcons(),
+                            buttonMode: ButtonMode.MATERIAL),
+                      ),
+                    )
+                  : SizedBox(),
+            ),
+          ],
+        ),
       ),
     );
   }
