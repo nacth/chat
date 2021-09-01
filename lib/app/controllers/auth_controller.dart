33522568d59fd79e6b15d1939edef3377c3bc552
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-09-01 13:59:19 +0700

    add firebase authentication

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -9,16 +10,28 @@ class AuthController extends GetxController {
 
   GoogleSignIn _googleSignIn = GoogleSignIn();
   GoogleSignInAccount? _currentUser;
+  UserCredential? _userCredential;
 
   void login() async {
     try {
+      await _googleSignIn.signOut();
       await _googleSignIn.signIn().then((value) => _currentUser = value);
-      await _googleSignIn.isSignedIn().then((value) {
-        if (value) {
-          isAuth.value = true;
-          Get.offAllNamed(Routes.HOME);
-        } else {}
-      });
+      final isSignIn = await _googleSignIn.isSignedIn();
+      if (isSignIn) {
+        print("200");
+        final googleAuth = await _currentUser?.authentication;
+        final credential = GoogleAuthProvider.credential(
+          idToken: googleAuth?.idToken,
+          accessToken: googleAuth?.accessToken,
+        );
+        await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .then((value) => _userCredential = value);
+        isAuth.value = true;
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        print("404");
+      }
     } catch (error) {
       print(error);
     }
